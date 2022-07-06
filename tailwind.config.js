16d3ab5d23f19bc40852ffa43c2ec9bd967c6c87
Akshay Khot <akshay.khot@cambridgebrainsciences.com>
2022-07-05 17:49:35 -0700

    cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,7 @@ module.exports = {
   content: ["./index.html"],
   theme: {
     extend: {
-      typography: {
-        DEFAULT: {
-          css: {
-            'blockquote p:first-of-type::before': { 
-              content: 'none' 
-            },
-            'blockquote p:first-of-type::after': { 
-              content: 'none' 
-            },
-          },
-        },
-      },
+      // add customizations here
     },
   },
   plugins: [
